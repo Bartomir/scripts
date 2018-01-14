5eab4583da993d2374a67a2d6b47e76aa186459f
Bartomir <bartosz.jeske@gmail.com>
2018-01-14 14:15:18 +0100

    grouper version 1

---
+++ Grouper.py
@@ -0,0 +1,33 @@
+import itertools
+import random
+
+name_list = []
+for num in itertools.count():
+    decision = input('Enter a name or type "no" to stop:')
+    if decision == 'no':
+        break
+    else:
+        name_list.append(decision)
+
+print(name_list)
+groups_number = int(input('Pick number of groups:'))
+
+all_groups = {}
+for number in range(groups_number):
+    all_groups['Group ' + str(number + 1)] = []
+
+counter = 0
+while len(name_list) > 0:
+    if len(name_list) >= groups_number:
+        for group, player in all_groups.items():
+            player.append(name_list.pop(random.randrange(len(name_list))))
+            counter += groups_number
+    elif len(name_list) < groups_number:
+        while len(name_list) < groups_number:
+            name_list.append('')
+            if len(name_list) == groups_number:
+                break
+        if len(name_list) == counter:
+            break
+
+print (all_groups)
