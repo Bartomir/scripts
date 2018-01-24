79b2e046b11edf7a822a8f8702031b549f8e10ca
Bartomir <bartosz.jeske@gmail.com>
2018-01-24 19:15:07 +0100

    added changes requested in CR

--- Grouper.py
+++ Grouper.py
@@ -8,24 +8,17 @@ while True:
     name_list.append(decision)
 
 print(name_list)
+random.shuffle(name_list)
+
 groups_number = int(input('Pick number of groups:'))
 
-all_groups = {}
-for number in range(groups_number):
-    all_groups['Group ' + str(number + 1)] = []
+all_groups = {'Group {}'.format(i + 1): [] for i in range(groups_number)}
 
-counter = 0
-while len(name_list) > 0:
-    if len(name_list) >= groups_number:
-        for group, player in all_groups.items():
-            player.append(name_list.pop(random.randrange(len(name_list))))
-            counter += groups_number
-    elif len(name_list) < groups_number:
-        while len(name_list) < groups_number:
-            name_list.append('')
-            if len(name_list) == groups_number:
-                break
-        if len(name_list) == counter:
+while name_list:
+    for group in all_groups.values():
+        try:
+            group.append(name_list.pop())
+        except IndexError:
             break
 
 print (all_groups)
